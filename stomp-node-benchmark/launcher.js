5d9c87fad5a3cfc07d52b2d4a9593afdca305752
Hiro Kurosu <hkurosu@gmail.com>
2016-12-27 22:32:06 -0800

    code clean-up.

--- stomp-node-benchmark/launcher.js
+++ stomp-node-benchmark/launcher.js
@@ -1,11 +1,17 @@
 var argv = require('minimist')(process.argv.slice(2), {default: {c:1,n:100}});
+
+var script = argv.s;
+if (script == null) {
+    usage();
+    return;
+}
 var concurrency = argv.c;
 var repeat = argv.n;
 
 var exec = require('child_process').exec;
 
 for (var i = 0; i < concurrency; ++i) {
-    exec('node ./stomp-node.js -n ' + repeat, function(error, stdout, stderr) {
+    var child = exec('node ' + script + ' -n ' + repeat, function(error, stdout, stderr) {
         console.log(stdout);
         if (stderr) {
             console.log('stderr:\n', stderr);
@@ -14,4 +20,9 @@ for (var i = 0; i < concurrency; ++i) {
             console.log('exec error:\n', error);
         }
     });
+    //console.log('['+new Date().toISOString()+'] started child process['+(i+1)+']: ' + child.pid);
+}
+
+function usage() {
+    console.log('TODO');
 }
---
+++ stomp-node-benchmark/stomp.js
@@ -0,0 +1,74 @@
+var argv = require('minimist')(process.argv.slice(2), {default: {n:100,m:10}});
+var repeat = argv.n;
+var limit = argv.m;
+
+var Stomp = require('stompjs');
+var client = Stomp.overWS("ws://localhost:8080/stomp/broker");
+
+if (argv.d) {
+    client.debug = function (message) {
+        console.log(message);
+    }
+}
+
+var Promise = require('promise');
+
+var connect = new Promise(function(resolve, reject) {
+    client.connect({}, resolve, reject);
+});
+
+var count = 0;
+var received = 0;
+
+var subscribe = function() {
+    client.subscribe('/user/queue/health', function (message) {
+        // console.log('Received: ' + message.body);
+        ++received;
+        --count;
+        // message.ack();
+    }, {ack: 'client'});
+    // console.log('Subscribing /user/queue/health');
+};
+
+var startTime;
+
+var send = function(n) {
+    startTime = new Date();
+    console.log('[' + startTime.toISOString() + '] sending ' + n + ' times.');
+    for (var i = 0; i < n; ++i) {
+        client.send('/app/health', {}, 'Hello');
+        // console.log('Sent Hello');
+        ++count;
+    }
+}
+
+var receive = function(startTime) {
+    (function check() {
+        var now = new Date();
+        if (count <= 0 || startTime.getTime() + limit * 1000 < now.getTime()) {
+            console.log('[' + now.toISOString() + '] '
+                + received + ' received ('
+                + (now.getTime() - startTime.getTime()) + ' ms.)');
+            client.disconnect();
+        } else {
+            setImmediate(check);
+        }
+    })();
+}
+
+connect                  // connect()
+    .then(function() {   // subscribe()
+        subscribe(); 
+    }).then(function() { // send() messages
+        send(repeat);
+    }).then(function() { // wait for all subscription messages
+        receive(startTime);
+    }
+);
+
+
+
+
+
+
+
